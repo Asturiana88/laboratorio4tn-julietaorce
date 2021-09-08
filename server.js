eb0099fab77639785df874c091f76b40a762c552
julieta <julietaorce@gmail.com>
2021-09-08 23:05:05 +0100

    fixes

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(
-    path.join(__dirname, 'dist', 'PrimerProyectoTest')));
+    path.join(__dirname, 'dist', 'angularapp')));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'PrimerProyectoTest', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'angularapp', 'index.html'));
 });
 console.log('Build successful!!');
